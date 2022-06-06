aaa62b17430af6ae2c57c2a824f60a5d32ca52ce
Caleb Archer <calebandcorey@gmail.com>
2022-06-06 16:25:24 +1200

    Add service and model classes

---
+++ Compx323Project/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compx323Project.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public DateTime ListingDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Publisher Publisher { get; set; }
+        public string AgeRating { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Compx323Project/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compx323Project.Models
+{
+    internal class Review
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public string Reviewer { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
